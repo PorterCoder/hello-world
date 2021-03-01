07bfcf43675e220b1c447479d06b80b7ca97071c
PorterCoder <aportercoder@gmail.com>
2021-03-01 15:32:25 -0700

    Update GitDemo.java
    Accidentally reversed my change of switching the two functions when resolving conflicts, so committing another change to put that back.

--- GitDemo.java
+++ GitDemo.java
@@ -19,6 +19,22 @@ public class GitDemo {
     }
  
     /*****
+     * This function should reverse 'num' and return the resulting value.
+     * i.e. if num = 1234
+     *  the function will return 4321
+     *****/
+    public double reverseNumber(int num){
+        double newNumber = 1;
+        
+        for(int i = 1; i <= num; ++i){
+            newNumber = newNumber * i;
+        }
+        
+        reverseNum = newNumber;
+        return newNumber;
+    }
+    
+    /*****
      * This function should calculate the factorial of 'num' and return the resulting value.
      * i.e. if num = 4,
      *  the function will return the value of (1 * 2 * 3 * 4)
@@ -35,19 +51,5 @@ public class GitDemo {
         return factorialNumber;
     }
     
-    /*****
-     * This function should reverse 'num' and return the resulting value.
-     * i.e. if num = 1234
-     *  the function will return 4321
-     *****/
-    public double reverseNumber(int num){
-        double newNumber = 1;
-        
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
-        }
-        
-        reverseNum = newNumber;
-        return newNumber;
-    }
+    
 }
